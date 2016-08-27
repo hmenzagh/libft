64b4560a3524d70c168eebeacb8bacfea8ad8bb5
hmenzagh <hmenzagh@student.42.fr>
2016-08-27 13:20:03 +0200

    strsub

---
+++ ft_lib/P2/ft_strnequ.c
@@ -0,0 +1,15 @@
+#include	<stddef.h>
+
+int		ft_strnequ(char const *s1,char const *s2, size_t n)
+{
+	int	i;
+
+	i = 0;
+	while(s1[i] && s2[i] && i < n)
+	{
+		if(s1[i] != s2[i])
+			return (0);
+		++i;
+	}
+	return (1);
+}
--- includes/libft.h
+++ includes/libft.h
@@ -40,7 +40,6 @@ int		ft_isprint(int c);
 char	*ft_strrchr(const char *s, int c); 
 void	*ft_memccpy(void *restrict dst, const void *restrict src, int c,
 			size_t n);
-
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
 char	*ft_strnew(size_t size);
@@ -50,5 +49,8 @@ void	ft_striter(char *s, void (*f)(char*));
 void 	ft_striteri(char *s,void (*f)(unsigned int, char*));
 char	*ft_strmap(char const *s,char (*f)(char));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strequ(char const *s1, char const *s2);
+int		ft_strnequ(char const *s1, char const *s2, size_t n);
+char	*ft_strsub(char const *s, unsigned int start, sizet_t len);
 
-#endif
+#endif 
