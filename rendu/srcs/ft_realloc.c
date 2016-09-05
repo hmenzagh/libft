d0754cca82b599fdfa19387a1346b3c69aec20d0
hmenzagh <hmenzagh@student.42.fr>
2016-09-05 12:40:37 +0200

    correc realloc

--- rendu/srcs/ft_realloc.c
+++ rendu/srcs/ft_realloc.c
@@ -9,5 +9,5 @@ void	ft_realloc(void **data, size_t cp_len, size_t size_new)
 	cp_len = (cp_len < size_new) ? size_new : cp_len;
 	ft_memcpy(ptr, *data, cp_len);
 	free(*data);
-	data = ptr;
+	*data = ptr;
 }
